edfa15fb786c625d35e0401048cc0d2c3c6acba0
ptums <ptumulty923@gmail.com>
2024-03-18 13:24:29 -0400

    refactored other templates to pull from data.json

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,9 @@
+var fs = require("fs");
+var pageNews = JSON.parse(fs.readFileSync("./data/news.json", "utf-8"));
+
 /* GET News page */
 const news = (req, res) => {
-  res.render("news", { title: "Travlr Getaways" });
+  res.render("news", { title: "Travlr Getaways", pageNews });
 };
 
 module.exports = {
